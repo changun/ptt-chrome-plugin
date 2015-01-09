fcf466a0a017361831ec4cad6f2823ad56843fd6
Larry <larry.lo1025@gmail.com>
2015-01-10 00:32:14 +0800

    Update firefox in master branch

--- ptt-plugin-firefox/data/assets/javascripts/contentscript.js
+++ ptt-plugin-firefox/data/assets/javascripts/contentscript.js
@@ -1,7 +1,19 @@
+// chrome extension
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-58095038-1']);
+_gaq.push(['_trackPageview']);
 
+// body
 var $body = $('body');
-var site = "//lifestreams.smalldata.io/ptt";
-var url = new URI(window.location.href).hostname();
+// the search endpoint
+var searchEndpointUrl = "//www.ptt.rocks/search";
+// url to get the post
+var postUrl = function(id){
+    var board_id = id.split(":");
+    var board = board_id[0];
+    var file = board_id[1] + ".html";
+    return "//s3-us-west-2.amazonaws.com/ptt.rocks/posts/" + board + '/' + file;
+};
 
 // the following DOM variables need to be initialized using jQuery selector.
 var $headline,// the DOM that contains the title of the article.
@@ -12,14 +24,33 @@ var $headline,// the DOM that contains the title of the article.
 var headlineTop, // the initial top value of the launcher button,
     launcherTopPos; // the top value of the launcher button when it has been squeezed to the upper right corner due to scrolling;
 
+/** flags for synchronization **/
+
+var _domCreated;
+var _responseReceived;
+var _available;
+var _init;
+
+function isComplete(){
+    // run init function when both ajax request and DOM creation are completed
+    if(_responseReceived && _domCreated && !_init){
+        _init = true;
+        init(response);
+    }
+}
 
 /** Initialization for each specific News Website**/
+var url = new URI(window.location.href).hostname();
 if(url.indexOf('yahoo') >=0) {
     // yahoo
      $headline = $(".headline");
      headlineTop = $headline.position()["top"];
      $articleContent = $(".yom-art-content p");
-     launcherTopPos = 31;
+    if($("#UH").length){
+        launcherTopPos = $("#UH").height();
+    }else{
+        launcherTopPos = 31;
+    }
 }else if (url.indexOf('nownews') >= 0){
     // NowNews
      $headline = $('.news_story h1');
@@ -48,16 +79,37 @@ if(url.indexOf('yahoo') >=0) {
     $articleContent = $('#newstext p');
     launcherTopPos = 10;
 }else if(url.indexOf('apple') >= 0){
-    // apple daily
+    // apple ent
     $headline = $('#h1');
-    headlineTop = $headline.position()["top"] + 10;
-    $articleContent = $("#summary");
+    if (url.indexOf('ent') >= 0) {
+        headlineTop = $headline.position()["top"] + 10;
+        $articleContent = $(".articulum p, .articulum h2");
+        launcherTopPos = 10;
+    } 
+    // apple daily
+    else {
+        headlineTop = $headline.position()["top"] + 10;
+        $articleContent = $("#summary"); 
+        launcherTopPos = 10;
+    }
+}else if(url.indexOf("ettoday") >= 0){
+    //ettoday
+    $headline = $('h2.title');
+    headlineTop = $headline.offset()["top"] + 10;
+    $articleContent = $("div.story sectione > p");
+    launcherTopPos = 10;
+}else if(url.indexOf("storm") >= 0){
+    //stormmedis
+    $headline = $('h3');
+    headlineTop = $headline.offset()["top"] + 10;
+    $articleContent = $(".newsDescBlk p");
     launcherTopPos = 10;
 }
 
 
 
-/** the parameter that will be sent to the search engine **/
+
+/** the parameters that will be sent to the search engine **/
 var title = $headline.text();
 var articleContent = $articleContent.text();
 var request = {"title": title,
@@ -69,33 +121,32 @@ $.ajax
 ({
     type: "POST",
     //the url where you want to sent the userName and password to
-    url: site + '/search',
+    url: searchEndpointUrl,
     async: true,
     //json object to sent to the authentication url
     data: JSON.stringify(request),
     success: function (ret) {
         response = ret;
+        _responseReceived = true;
         // run init function if the dom creation is also completed
         isComplete();
     }
 });
-var launcherTitle = '<i class="announcement icon"></i><span class="ui-text">看看鄉民怎麼說?</span>';
 
 
 
 /** create plugin DOMs **/
 
-var _domCreated;
 // launcher
-var $leftLauncher = $('<div id="launcher" class="ui black small launch right attached fixed transition hidden button" style="font-size: 15px">'+launcherTitle+'</div>');
+var $leftLauncher = $('<div id="launcher" class="ui black small launch right attached fixed transition hidden button" style="font-size: 15px">\n    <div><i class="announcement icon"></i><span class="ui-text" id="launcher-title">看看鄉民怎麼說?</span></div>\n    \n    </div>');
 // move launcher button to be vertically aligned with the article headline
 $leftLauncher.css({top: headlineTop});
 
 // sidebar menu
-var $menu = $('<div class="ui vertical plugin-menu sidebar inverted very wide right">\n    <div class="header item">\n        <i id="lock" class="icon unlock alternate"></i>\n        相關文章\n    </div>\n\n</div>\n        ');
+var $menu = $('<div class="ui vertical plugin-menu sidebar inverted very wide right">\n\n</div>\n        ');
 
 // modal
-var $modal =$('<div class="ui modal" id="ptt-modal"><i class="close icon"></i></div>');
+var $modal =$('<div class="ui modal  large" id="ptt-modal"><i class="close icon"></i></div>');
 // append the DOMs to the body
 $body.append($menu);
 $body.append($leftLauncher);
@@ -114,26 +165,48 @@ $menu
         }
     });
 
-// set on mouseover handler
-$leftLauncher.on('mouseover', function(event) {
 
-    $menu.sidebar('show');
-    $leftLauncher.addClass('hidden');
-    event.preventDefault();
-});
+var setMouseOverEvent = function(isAvailable){
+    if(isAvailable) {
+        // set on mouseover handler
+        $leftLauncher.on('mouseover', function (event) {
+            $menu.sidebar('show');
+            $leftLauncher.addClass('hidden');
+            event.preventDefault();
+            _gaq.push(['_trackEvent', "launcherButton", 'mouseover']);
+        });
+    }else{
+        var $launcherTitle = $("#launcher-title");
+        var $launcherIcon = $(".icon.announcement");
+        var originalTitle = $launcherTitle.html();
+        $leftLauncher.on('mouseover', function (event) {
+            $launcherTitle.html("暫無相關評論");
+            $launcherIcon.removeClass("announcement");
+            $launcherIcon.addClass("frown");
+            event.preventDefault();
+        });
+        $leftLauncher.on('mouseout', function (event) {
+            $launcherTitle.html(originalTitle);
+            $launcherIcon.addClass("announcement");
+            $launcherIcon.removeClass("frown");
+            event.preventDefault();
+        });
+    }
+}
 
 // set on scroll listener to dynamically adjust the position of the launcher
 $(window).on("scroll", function(e) {
     var scrollTop = $body.scrollTop();
-    if (scrollTop > headlineTop && !$leftLauncher.hasClass('fixed-top')) {
+    if (headlineTop - scrollTop <=  launcherTopPos && !$leftLauncher.hasClass('fixed-top')) {
         $leftLauncher.addClass("fixed-top");
         $leftLauncher.css({top: launcherTopPos});
 
-    } else if($body.scrollTop() <= headlineTop && $leftLauncher.hasClass('fixed-top')){
+    } else if(headlineTop - scrollTop > launcherTopPos && $leftLauncher.hasClass('fixed-top')){
         $leftLauncher.removeClass("fixed-top");
         $leftLauncher.css({top: headlineTop});
     }
 });
+
 
 // this prevents the menu's scrolling from effecting the whole window's scrolling
 // IT REQUIRES jquery.mousewheel.js to work!
@@ -150,47 +223,66 @@ _domCreated = true;
 // check if both DOM creation and ajax request are completed
 isComplete();
 
-var _init;
-function isComplete(){
-    // run init function when both ajax request and DOM creation are completed
-    if(response && _domCreated && !_init){
-        _init = true;
-        init(response);
-    }
-}
-var showPost = function (event){
-    var uri = site + "/posts/" + $(this).data('post');
-    $.ajax
-    ({
-        type: "GET",
-        //the url where you want to sent the userName and password to
-        url: uri,
-        async: true,
-        success: function (ret) {
-            var $postContent = $(ret).find('#main-content');
-            var $prevPostContnet = $modal.find('#main-content');
-            if($prevPostContnet.length){
-                $prevPostContnet.replaceWith($postContent);
-            }else{
-                $modal.append($postContent);
-            }
 
-            $modal
-                .modal('setting', 'transition', 'horizontal flip')
-                .modal('show')
-            ;
-        }
-    });
-
-}
 function init(ret){
+    // show the ptt post in the modal when the user clicks a post
+    var showPost = function (event){
+        var uri = postUrl($(this).data('id'));
+        $.ajax
+        ({
+            type: "GET",
+            //the url where you want to sent the userName and password to
+            url: uri,
+            async: true,
+            success: function (ret) {
+                var $postContent = $(ret).find('#main-content');
+                var $prevPostContnet = $modal.find('#main-content');
+                if($prevPostContnet.length){
+                    $prevPostContnet.replaceWith($postContent);
+                }else{
+                    $modal.append($postContent);
+                }
+                $modal
+                    .modal('setting', 'transition', 'horizontal flip')
+                    .modal('show')
+                ;
+                _gaq.push(['_trackEvent', "showPost", 'click', uri]);
+            }
+        });
+
+    };
     /** populate the sidebar content **/
+    // populate related articles
+    if(ret["excellent-articles"] && ret["excellent-articles"].length > 0){
+        _available = true;
+        var header =
+            $('<div class="header item" id="excellent-article-header"><span class="sub header">精選文章</span></div>')
+                .appendTo($menu);
+        ret["excellent-articles"].forEach(function(e){
+            var title = e.title ? e.title : e.subject;
+            var ago = moment(e.last_modified).locale("zh-tw").fromNow();
+            var popularityTag = '<span class="push-count f4 hl">優</span>';
+
+            $('<a class="post item r-ent"></a>')
+                .append( popularityTag)
+                .append('<span class="ui title">' + title +'</span>')
+                .append('<span class="ui date detail"> ' + ago +'</span>')
+                .data("id", e.id)
+                .appendTo($menu)
+                .click(showPost);
+        });
+
+
+    }
+    /** populate the sidebar content **/
+    // populate related articles
     if(ret.articles.length > 0){
+        _available = true;
+        var header =
+            $('<div class="header item">\n    相關文章\n</div>')
+                .appendTo($menu);
         ret.articles.forEach(function(e){
             var board_id = e.id.split(":");
-            var board = board_id[0];
-            var file = board_id[1] + ".html";
-            var href = 'https://www.ptt.cc/bbs/'+ board + '/'+ file;
             var title = e.title ? e.title : e.subject;
             var ago = moment(e.last_modified).locale("zh-tw").fromNow();
             var popularity;
@@ -205,78 +297,86 @@ function init(ret){
                 .append(popularity)
                 .append('<span class="ui title">' + title +'</span>')
                 .append('<span class="ui date detail"> ' + ago +'</span>')
-                .data("post", board + '/' + board_id[1])
+                .data("id", e.id)
                 .appendTo($menu)
                 .click(showPost);
         });
 
 
     }
-    if(ret.comments){
-        var board_id = ret.comments._id.split(":");
-        var board = board_id[0];
+    // populate comment
+    if(ret["best-match"]){
+        _available = true;
+        var id = ret["best-match"].id;
+        var title = ret["best-match"].title;
+        var ago = moment(ret["best-match"].last_modified).locale("zh-tw").fromNow();
         var header =
-                $('<div class="header item" id="comment-header"><i class="comment icon"></i><span class="sub header"> '+ret.comments.title+'</span></div>')
+                $('<div class="header item r-ent" id="comment-header"><span class="sub header"> '+title+'</span><span class="ui date detail">' + ago +'</span></div>')
                 .appendTo($menu)
-                .data("post", board + '/' + board_id[1])
-                .click(showPost)
-            ;
-        var count = 0;
-        ret.comments.pushed.forEach(function(e){
-            count ++;
-            if(count > 50){
-                return null;
+                .data("id", id)
+                .click(showPost);
+
+        // fetch the post from S3
+        $.ajax
+        ({
+            type: "GET",
+            //the url where you want to sent the userName and password to
+            url: postUrl(id),
+            async: true,
+            success: function (ret) {
+                var $post = $(ret);
+                // remove push times
+                $post.find("span.push-ipdatetime").remove();
+                // populate each push div to the sidebar
+                $post.find("div.push").each(function(index){
+                    var $pushDiv = $(this);
+                    $('<a class="item push"></a>')
+                        .append($pushDiv.children())
+                        .data("id", id)
+                        .appendTo($menu);
+
+                });
+                // add an empty element to the bottom of the menu to fix the scroll bar issue
+                $('<a class="item"></a>')
+                    .appendTo($menu);
             }
-            var tag = null;
-            if(e.op == "推"){
-                tag = '<span class="hl push-tag">推</span>';
-            }else if(e.op == "→"){
-                tag = '<span class="f1 hl push-tag">→</span>';
-            }else if(e.op == "噓"){
-                tag = '<span class="f1 hl push-tag">噓</span>';
-            }
-            var content = URI.withinString(e.content, function(url) {
-                return '<a target="_blank" href="'+url+'">' + url + '</a>';
-            });
-            $('<a class="item push"></a>')
-                .append(tag)
-                .append('<span class="f3 hl push-userid">' + e.user +'</span>')
-                .append('<span class="f3 push-content">: ' + content +'</span>')
-                .appendTo($menu);
         });
-        // add an empty element to the bottom to fix the scroll bar issue
-        $('<a class="item"></a>')
-            .appendTo($menu);
 
     }
+    // add Lock icon to the first header in the sidebar
+    $(".ui.plugin-menu .header").first().append('<i id="lock" class="icon unlock alternate"></i>');
+    setMouseOverEvent(_available);
+
+    _gaq.push(['_trackEvent', "launcherButton", 'loaded']);
 
     /** Maintain the locking function **/
-    // var $lock = $("#lock");
-
+    var $lock = $("#lock");
     // check if the sidebar has been locked by the user
     // chrome.storage.local.get({'lock?': 'unlocked'}, function(ret) {
-    //     // automatically show the sidebar if the user has chosen to lock it
-    //     if(ret["lock?"] == 'locked'){
-    //         $lock.removeClass('unlock').addClass('lock');
-    //         $menu.sidebar('show');
-    //         $leftLauncher.addClass('hidden');
-    //     }else{
-    //         /** Fade-in the launcher **/
-            $leftLauncher.transition('horizontal flip', '1000ms');
-    //     }
+        // automatically show the sidebar if the user has chosen to lock it
+        if(ret["lock?"] == 'locked' && _available){
+            $lock.removeClass('unlock').addClass('lock');
+            $menu.sidebar('show');
+            $leftLauncher.addClass('hidden');
+        }else{
+                $.later(100, this, function () {
+                    /** Fade-in the launcher **/
+                    $leftLauncher.transition('horizontal flip', '1000ms');
+                });
+        }
     // });
-    // $lock.click(function(event){
-    //     var locked = !$lock.hasClass('lock');
-    //     if(locked){
-    //         $lock.removeClass('unlock').addClass('lock');
+    $lock.click(function(event){
+        var locked = !$lock.hasClass('lock');
+        if(locked){
+            $lock.removeClass('unlock').addClass('lock');
 
-    //     }else{
-    //         $lock.removeClass('lock').addClass('unlock');
-    //     }
-    //     chrome.storage.local.set({'lock?': locked? 'locked': 'unlocked'}, function(){
-    //         console.log(locked);
-    //     });
-    // });
+        }else{
+            $lock.removeClass('lock').addClass('unlock');
+        }
+        // chrome.storage.local.set({'lock?': locked? 'locked': 'unlocked'}, function(){
+        //     console.log(locked);
+        // });
+    });
 
 
 }
--- ptt-plugin-firefox/lib/main.js
+++ ptt-plugin-firefox/lib/main.js
@@ -7,32 +7,39 @@ var self = require("sdk/self");
 // The script replaces the page contents with a message
 pageMod.PageMod({
     include: [
-        "http://tw.news.yahoo.com/*",
-        "https://tw.news.yahoo.com/*",
-        "http://www.nownews.com/n/*",
-        "https://www.nownews.com/n/*",
-        "http://www.chinatimes.com/realtimenews/*",
-        "https://www.chinatimes.com/realtimenews/*",
-        "http://udn.com/*",
-        "https://udn.com/*",
-        "http://news.ltn.com.tw/news/*",
-        "https://news.ltn.com.tw/news/*",
-        "http://www.appledaily.com.tw/realtimenews/article/*",
-        "https://www.appledaily.com.tw/realtimenews/article/*"
-        ],
+      "http://tw.news.yahoo.com/*",
+      "https://tw.news.yahoo.com/*",
+      "http://www.nownews.com/n/*",
+      "https://www.nownews.com/n/*",
+      "http://www.chinatimes.com/realtimenews/*",
+      "https://www.chinatimes.com/realtimenews/*",
+      "http://www.chinatimes.com/newspapers/*",
+      "https://www.chinatimes.com/newspapers/*",
+      "http://udn.com/*",
+      "https://udn.com/*",
+      "http://news.ltn.com.tw/news/*",
+      "https://news.ltn.com.tw/news/*",
+      "http://www.appledaily.com.tw/*",
+      "https://www.appledaily.com.tw/*",
+      "http://ent.appledaily.com.tw/enews/*",
+      "http://www.ettoday.net/news/*",
+      "https://www.ettoday.net/news/*",
+      "http://www.stormmediagroup.com/opencms/*",
+      "htts://www.stormmediagroup.com/opencms/*"    
+    ],
     contentStyleFile: [
-        self.data.url("assets/css/bbs.css"),
-        self.data.url("assets/css/plugin.css"),
         self.data.url("assets/css/semantic.css"),
-        self.data.url("assets/css/semantic.min.css")
+        self.data.url("assets/css/plugin.css"),
+        self.data.url("assets/css/bbs.css")
     ],
     contentScriptFile: [
+        self.data.url("assets/javascripts/ga.js"),
         self.data.url("assets/javascripts/jquery-2.1.3.min.js"), 
         self.data.url("assets/javascripts/jquery.mousewheel.js"),
-        self.data.url("assets/javascripts/semantic.js"), 
+        self.data.url("assets/javascripts/jquery.later.js"),
+        self.data.url("assets/javascripts/semantic.js"),
         self.data.url("assets/javascripts/moment-with-langs.min.js"), 
-        self.data.url("assets/javascripts/URI.js"),
+        self.data.url("assets/javascripts/URI.js"), 
         self.data.url("assets/javascripts/contentscript.js") 
-        ]
-
+    ]
 });
